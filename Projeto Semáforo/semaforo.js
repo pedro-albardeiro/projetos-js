08901b54e823a817252f0e88d1c7684c9507b1cb
thewhoami7 <pedropubgg@gmail.com>
2025-02-03 20:16:00 -0300

    Projeto Semáforo Em JS

---
+++ Projeto Semáforo/semaforo.js
@@ -0,0 +1,36 @@
+const img = document.getElementById('img');
+const buttons = document.getElementById('buttons');
+let colorIndex = 0;
+let intervalId = null;
+
+const trafficLight =  ( event ) => {
+    turnOn[event.target.id]();
+}
+
+const nextIndex = () => {
+    if ( colorIndex < 2 ) {
+        colorIndex++
+    }else {
+        colorIndex = 0;
+    }
+}
+
+const changeColor = () => {
+      const colors = ['red', 'yellow', 'green']
+      const color = colors[ colorIndex ];
+      turnOn[color]();
+      nextIndex();
+}
+
+const stopAutomatic = ()
+
+const turnOn = {
+    'red':    () => img.src = './img/vermelho.png',
+    'yellow': () => img.src = './img/amarelo.png',
+    'green': () => img.src = './img/verde.png',
+    'automatic': () => setInterval(changeColor, 1000)
+}
+
+buttons.addEventListener('click', trafficLight);
+
+/* quando alguem clicar em um dos buttons ele vai capturar o click */
